9aa324fa47acbcc6487a0c4c4380cedb45092dad
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-19 10:35:00 +0300

    Finish 1. Introduction

--- The Hard Parts of Object Oriented JavaScript/1. Introduction/Slides.js
+++ The Hard Parts of Object Oriented JavaScript/1. Introduction/Slides.js
@@ -1,1 +1,16 @@
 /* Introduction
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+OOP - an enormously popular paradigm for structuring our
+complex code
+— Easy to add features and functionality
+— Easy for us and other developers to reason about (a
+clear structure)
+— Performant (efficient in terms of memory)
+We need to organize our code as it gets more complex
+so it's not just an endless series of commands
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
