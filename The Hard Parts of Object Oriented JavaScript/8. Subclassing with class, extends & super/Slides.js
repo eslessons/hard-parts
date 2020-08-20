895b8f938364848317c1c89fd90d2326a3cffd2e
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-20 14:09:41 +0300

    Finish 8. Subclassing with class, extends, super

--- The Hard Parts of Object Oriented JavaScript/8. Subclassing with class, extends & super/Slides.js
+++ The Hard Parts of Object Oriented JavaScript/8. Subclassing with class, extends & super/Slides.js
@@ -1,1 +1,44 @@
 /* Subclassing with class, extends & super
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Subclassing in Solution 4
+ES2015 Class approach
+
+class userCreator{
+  constructor (name, score) {
+    this.name = name;
+    this.score = score;
+  }
+  sayName () {
+    console.log("I am "+this.name);
+  }
+  increment () {
+    this.score++;
+  }
+}
+
+const user1 = new userCreator("Phil", 4);
+const user2 = new userCreator("Tim", 4);
+
+user1.sayName()
+
+class paidUserCreator extends userCreator {
+  constructor(paidName, paidScore, accountBalance) {
+    super (paidName, paidScore)
+    this.accountBalance = accountBalance;
+  }
+  increaseBalance() {
+    this.accountBalance++
+  }
+}
+
+const paidUser1 = new paidUserCreator("Alyssa", 8, 25);
+
+paidUser1.increaseBalance();
+
+paidUser1.sayName();
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
