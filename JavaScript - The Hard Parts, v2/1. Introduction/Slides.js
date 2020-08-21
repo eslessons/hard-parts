9d413398344f0824526510d8682f0cad62621d60
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-21 09:57:53 +0300

    Finish 2. JavaScript Principles

--- JavaScript - The Hard Parts, v2/1. Introduction/Slides.js
+++ JavaScript - The Hard Parts, v2/1. Introduction/Slides.js
@@ -9,4 +9,14 @@ Classes/Prototypes & Asynchronicity
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+Contents
+
+1. Principles of JavaScript
+2. Callbacks & Higher order functions
+3. Closure (scope and execution context)
+4. Asynchronous JavaScript & the event loop
+5. Classes & Prototypes (OOP)
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 */
--- JavaScript - The Hard Parts, v2/2. JavaScript Principles/Slides.js
+++ JavaScript - The Hard Parts, v2/2. JavaScript Principles/Slides.js
@@ -1,1 +1,74 @@
 /* JavaScript Principles
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+JavaScript principles
+
+When JavaScript code runs, it:
+
+Goes through the code
+line-by-line and runs/ ’executes’
+each line - known as the thread
+of execution
+
+Saves ‘data’ like strings and
+arrays so we can use that data
+later - in its memory
+We can even save code
+(‘functions’)
+
+const num = 3;
+function multiplyBy2 (inputNumber){
+const result = inputNumber*2;
+return result;
+}
+const output = multiplyBy2(num);
+const newOutput = multiplyBy2(10);
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Functions
+
+Code we save (‘define’) functions &
+can use (call/invoke/execute/run)
+later with the function’s name & ( )
+
+Execution context
+
+Created to run the code of a
+function - has 2 parts (we’ve
+already seen them!)
+- Thread of execution
+- Memory
+
+(We’ll see another way of defining functions later)
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Call stack
+
+- JavaScript keeps track of what
+function is currently running
+(where’s the thread of execution)
+
+- Run a function - add to call stack
+
+- Finish running the function - JS
+removes it from call stack
+
+- Whatever is top of the call stack
+  - that’s the function we’re currently running
+
+const num = 3;
+
+function multiplyBy2 (inputNumber){
+  const result = inputNumber*2;
+  return result;
+}
+
+const output = multiplyBy2(num);
+const newOutput = multiplyBy2(10);
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
