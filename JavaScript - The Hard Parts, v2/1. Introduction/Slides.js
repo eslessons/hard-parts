bd41c9afdde488f37e506fd618a01f277a24ae0b
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-21 09:26:23 +0300

    Finish 1. Introduction

---
+++ JavaScript - The Hard Parts, v2/1. Introduction/Slides.js
@@ -0,0 +1,12 @@
+/* Introduction
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+JavaScript the Hard Parts
+
+JavaScript principles, Callbacks & Higher Order functions, Closure,
+Classes/Prototypes & Asynchronicity 
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
