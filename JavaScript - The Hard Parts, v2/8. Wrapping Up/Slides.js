78990dc4ed04823d0c8ffa7529eac2ad62318197
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-23 17:55:38 +0300

    Finish 8. Wrapping Up

--- JavaScript - The Hard Parts, v2/8. Wrapping Up/Slides.js
+++ JavaScript - The Hard Parts, v2/8. Wrapping Up/Slides.js
@@ -1,2 +1,15 @@
 /* Wrapping Up
 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Fin
+
+1. Principles of JavaScript
+2. Callbacks & Higher order functions
+3. Closure (scope and execution context)
+4. Asynchronous JavaScript & the event loop
+5. Classes & Prototypes (OOP)
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+*/
