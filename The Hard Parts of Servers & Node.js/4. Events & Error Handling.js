13f8f17a38fa3bbfa1d91e441cea504d6e554f66
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-26 13:52:01 +0300

    Finish 4. Events & Error Handling

--- The Hard Parts of Servers & Node.js/4. Events & Error Handling.js
+++ The Hard Parts of Servers & Node.js/4. Events & Error Handling.js
@@ -1,2 +1,44 @@
 /* Events & Error Handling
 
+In server side development we get errors
+
+Understandable - we’re interacting with
+others’ computers over the internet - there’s
+lots of issues that could arise
+
+How can we handle this? We need to
+understand our background Node http server
+feature better
+
+What triggers the doOnIncoming function to
+run? Events
+
+	— http.createServer(doOnIncoming)
+	is actually a one-line version of setting up
+	the server that will auto-release (‘emit’)
+	events (‘messages in Node’) that trigger a
+	function to auto-run if we’ve set one
+
+	— These events are preset on http: ‘request’,
+	‘error’
+
+Node will automatically send out the
+appropriate event depending on what it gets
+from the computer internals (http message or
+error)
+
+function doOnIncoming(incomingData, functionsToSetOutgoingData){
+	functionsToSetOutgoingData.end("Welcome to Twitter")
+}
+
+function doOnError(infoOnError){
+ console.error(infoOnError)
+}
+
+const server = http.createServer();
+server.listen(80)
+
+server.on('request', doOnIncoming)
+server.on('clientError', doOnError)
+
+*/
