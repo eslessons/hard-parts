d2807f07a576b1f557116130456a7e941f14c696
eslessons <66363651+eslessons@users.noreply.github.com>
2020-08-27 12:27:15 +0300

    Finish 6. Streams

--- The Hard Parts of Servers & Node.js/6. Streams.js
+++ The Hard Parts of Servers & Node.js/6. Streams.js
@@ -1,1 +1,25 @@
 /* Streams
+
+What if Node used the ‘event’ (messagebroadcasting) pattern to send out a message (‘event’) each time a sufficient batch of the
+json datahad been loaded in
+
+And at each point, take that data and start cleaning it - in
+batches
+
+let cleanedTweets = "";
+
+function cleanTweets (tweetsToClean){
+	// algorithm to remove bad tweets from `tweetsToClean`
+}
+
+function doOnNewBatch(data){
+	cleanedTweets += cleanTweets(data);
+}
+
+const accessTweetsArchive = fs.createReadStream('./tweetsArchive.json')
+
+accessTweetsArchive.on('data', doOnNewBatch);
+
+Introducing Event loop and Callback Queue
+ 
+*/
